396677e8012ec18dae8e236ec82ac5ac1b421f8c
Rogerio pc Interativa <rogerioo.lara@hotmail.com>
2023-05-22 15:34:58 -0300

    commit sublime

--- listas-dinamicas/pilhas_dinamicas.c
+++ listas-dinamicas/pilhas_dinamicas.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Estrutura N�
+//Estrutura No
 typedef struct NO {
     int dado;
     struct NO* prox;
@@ -18,7 +18,7 @@ PILHA* p;
 void empilha() {
     //aloca o novo nó ptr de tamanho struct nó e atribui ao ponteiro ptr
     NO* ptr = (NO*)malloc(sizeof(NO));
-    //atribui um valor aleat�rio ao dado
+    //atribui um valor aleatorio ao dado
     ptr->dado = rand() % 100;
     //atualiza o ponteiro prox para ser o novo topo
     ptr->prox = p->topo;
@@ -39,14 +39,13 @@ int desempilha() {
         ptr->prox = NULL;
         //armazena o valor do retorno
         dado = ptr->dado;
-        //libera mem�ria / destroi o elemento
+        //libera memoria / destroi o elemento
         free(ptr);
         //retorno da funcao
         return dado;
     }
 }
 
-int main()
-{
+int main(){
     printf("Ola mundo");
 }
