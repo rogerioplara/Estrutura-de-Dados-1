9efd611d3fd45b5a79720d0231706a2234591ffb
Rogerio pc Interativa <rogerioo.lara@hotmail.com>
2023-05-22 17:45:36 -0300

    commit sublime

--- listas-dinamicas/filas_dinamicas.c
+++ listas-dinamicas/filas_dinamicas.c
@@ -1,6 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef struct NO{
+    int dado; //dado;
+    struct NO* prox; //ponteiro para o próximo elemento da fila;
+}NO;
+
+typedef struct FILA{
+    NO* inicio; //início da fila;
+    NO* fim; //fim da fila;
+}FILA;
+
+FILA *p; //instância do tipo fila
+
+void enfileira(){
+    NO* ptr = (NO *) malloc(sizeof(NO));
+    ptr->dado = rand()%100;
+    ptr->prox = NULL;
+    if(f->inicio == NULL)
+        f->inicio = ptr;
+    else
+        f->fim->prox = ptr;
+    f->fim = ptr;
+}
+
 int main(){
 
 }
