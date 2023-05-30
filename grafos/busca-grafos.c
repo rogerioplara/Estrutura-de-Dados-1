5b873bd3da4fc4fb36297f2e6b3f75724aa4b896
Rogerio pc Interativa <rogerioo.lara@hotmail.com>
2023-05-30 18:00:42 -0300

    busca em largura

--- grafos/busca-grafos.c
+++ grafos/busca-grafos.c
@@ -63,3 +63,58 @@ void buscaEmProfundidade(struct str_no g[], int inicio, int alvo){
     }
     return;
 }
+
+//busca em largura
+void buscaEmLargura(struct str_no g[], int inicio, int alvo){
+    int fila[MAXV]; //fila
+    bool visitado[MAXV]; //nos visitados
+    int indice = 0; //controle da fila
+    bool achou = false; //flag (nao visitados
+    int corrente = inicio;
+    struct str_no *ptr;
+    int i;
+    printf("=-=-=-= Busca em Largura =-=-=-=\n");
+    //marcando os nós como não visitados
+    for(i=0; i<MAXV; i++){
+        visitado[i] = false;
+    }
+    //partindo do primeiro vertice
+    printf("VISITANDO: %d. \n", corrente);
+    visitado[corrente] = true;
+    fila[indice] = corrente;
+    indice++;
+    while(true){
+        //visitar os nos adjacentes ao vertice corrente
+        for(ptr=g[corrente].proximo; ptr!=NULL; ptr = ptr->proximo){
+            //caso corrente ainda não tenha sido visitado:
+            corrente=ptr->id;
+            if(!visitado[corrente]){
+                //enfileira e marca como visitado
+                printf("VISITANDO: %d. \n", corrente);
+                if(corrente == alvo){
+                    printf("Alvo encontrado!\n\n\n");
+                    return;
+                }
+                visitado[corrente] = true;
+                fila[indice] = corrente;
+                indice++;
+            }
+        }
+        //caso a fila nao esteja vazia:
+        if(indice!=0){
+            //atualizando vertice corrente
+            corrente = fila[0];
+            //desenfileirando o primeiro vertice
+            for(i=1; i<indice+1; i++){
+                fila[i-1]=fila[i];
+            }
+            indice--;
+
+        } else {
+            //nao ha mais vertices para visitar
+            printf("Encerrando busca.\n");
+            break;
+        }
+    }
+    return;
+}
